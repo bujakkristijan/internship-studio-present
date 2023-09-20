512e74cfc5014c4ff29e2071db304514a96c3eae
Kristijan Bujak <cileb411@gmail.com>
2023-09-20 13:57:31 +0200

    hamburger menu zapocet i responsive design

--- index.js
+++ index.js
@@ -14,6 +14,49 @@ function updateSlidesPerView() {
 
   // Listen for window resize events and update slides-per-view
   window.addEventListener("resize", updateSlidesPerView);
+
+  function showMenu() {
+    const headerBoxTabContainer = document.getElementById("header-box-tab-container");
+    headerBoxTabContainer.style.right = "0px";
+    headerBoxTabContainer.style.boxShadow = "0 0 0 10000px rgba(0, 0, 0, .50)";
+    document.body.style.overflow = "hidden";
+  
+    // Get the x icon element
+    const xIcon = document.querySelector(".fa.fa-times");
+  
+    // Create a new child element for language selection
+    const languageMobileContainer = document.createElement("div");
+    languageMobileContainer.className = "language-mobile-container";
+  
+    // Create and append the 'Srpski' div
+    const srpskiDiv = document.createElement("div");
+    srpskiDiv.textContent = "Srpski";
+    languageMobileContainer.appendChild(srpskiDiv);
+  
+    // Create and append the 'English' div
+    const englishDiv = document.createElement("div");
+    englishDiv.textContent = "English";
+    languageMobileContainer.appendChild(englishDiv);
+  
+    // Create and append the 'Magyar' div
+    const magyarDiv = document.createElement("div");
+    magyarDiv.textContent = "Magyar";
+    languageMobileContainer.appendChild(magyarDiv);
+  
+    // Insert the language selection container after the x icon
+    headerBoxTabContainer.insertBefore(languageMobileContainer, xIcon.nextSibling);
+  
+    // Create the "Language" text container element
+    const languageTextContainer = document.createElement("div");
+    languageTextContainer.className = "language-text-container";
+    languageTextContainer.textContent = "Language";
+  
+    // Insert the "Language" text container before the language selection container
+    headerBoxTabContainer.insertBefore(languageTextContainer, languageMobileContainer);
+  }
+
+  let hamburgerMenuButton = document.getElementById("hamburger-menu-button");
+  hamburgerMenuButton.addEventListener('click', showMenu);
   
   
   
