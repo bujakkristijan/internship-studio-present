c3b6baebfd078a0eea71828b2b27d26d1d7d068a
Kristijan Bujak <cileb411@gmail.com>
2023-09-21 22:45:15 +0200

    uradjeno da se menjaju boje na toggle button kod vecine elemenata sto je trebalo, kod our-products nisam mogao jer je slika u stvari sa belom pozadinom i slikom proizvoda u njoj

--- index.js
+++ index.js
@@ -1,5 +1,18 @@
 const headerBoxTabContainer = document.getElementById("header-box-tab-container");
 const headerBoxLogoAndLanguage = document.querySelector(".header-box-1");
+const tabTitle = document.querySelector(".tab-title");
+const offerTitle = document.querySelector(".title-offer");
+const dvgNumbersTitle = document.querySelector('.dvg-numbers-title');
+const dvgNumbersSubTitle = document.querySelector('.dvg-numbers-subtitle');
+const ourProductsContainer = document.querySelector('.our-products-container');
+const ourProductsTitle = document.querySelector('.our-products-title');
+const ourProductsDescription = document.querySelector('.our-products-description');
+const ourBrandsTitle = document.querySelector('.our-brands-title');
+const ourBrandsImageTitle = document.querySelector('.our-brands-image-title');
+
+
+
+
 let hamburgerMenuButton = document.getElementById("hamburger-menu-button");
 let xMenuButton = document.getElementById("x-menu-button");
 
@@ -9,6 +22,11 @@ document.addEventListener("DOMContentLoaded", toggleSwitchMode)
 function toggleSwitchMode(){
   const darkModeToggle = document.getElementById('swith-toggle-mode');
   const body = document.body;
+
+  //premestio sam ovde da ne bi u konzoli greske ispisivao jer ne postoje na pocetku, vec se kreiraju preko js-a, mada je radilo okej sve 
+  const languageTitleMobile = document.querySelector('.language-text-container');
+  const languageTextMobile = document.querySelector('.language-mobile-container');  
+
   let isDarkModeEnabled = false;
 
     // Check the initial state of the dark mode toggle
@@ -30,11 +48,39 @@ function toggleSwitchMode(){
 
     function enableDarkMode() {
       body.classList.add('dark-mode');
+      tabTitle.classList.add('dark-mode');
+      offerTitle.classList.add('dark-mode');
+      dvgNumbersTitle.classList.add('dark-mode');
+      dvgNumbersSubTitle.classList.add('dark-mode');
+      ourProductsContainer.classList.add('dark-mode');
+      hamburgerMenuButton.classList.add('dark-mode');
+      xMenuButton.classList.add('dark-mode');
+      languageTitleMobile.classList.add('dark-mode');
+      languageTextMobile.classList.add('dark-mode');
+      ourProductsTitle.classList.add('dark-mode');
+      ourProductsDescription.classList.add('dark-mode');
+      ourBrandsTitle.classList.add('dark-mode');
+      ourBrandsImageTitle.classList.add('dark-mode');
+      
       // headerBoxLogoAndLanguage.classList.add('dark-mode'); /* ne mora ovako svaki posebno, izgleda je dosta na body sto ima classList. Proveriti posle zasto tako radi tacno!!! */ 
     }
 
     function disableDarkMode() {
       body.classList.remove('dark-mode');
+      tabTitle.classList.remove('dark-mode');
+      offerTitle.classList.remove('dark-mode');
+      dvgNumbersTitle.classList.remove('dark-mode');
+      dvgNumbersSubTitle.classList.remove('dark-mode');
+      ourProductsContainer.classList.remove('dark-mode');
+      hamburgerMenuButton.classList.remove('dark-mode');
+      xMenuButton.classList.remove('dark-mode');
+      languageTitleMobile.classList.remove('dark-mode');
+      languageTextMobile.classList.remove('dark-mode');
+      ourProductsTitle.classList.remove('dark-mode');
+      ourProductsDescription.classList.remove('dark-mode');
+      ourBrandsTitle.classList.remove('dark-mode');
+      ourBrandsImageTitle.classList.remove('dark-mode');
+      
       // headerBoxLogoAndLanguage.classList.remove('dark-mode');
     }
 
@@ -47,7 +93,7 @@ function updateSlidesPerView() {
     if (viewportWidth < 900) {
       swiperContainer.setAttribute("slides-per-view", "1");
     } else {
-      swiperContainer.setAttribute("slides-per-view", "3");
+      swiperContainer.setAttribute("slides-per-view", "4");
     }
   }
 
